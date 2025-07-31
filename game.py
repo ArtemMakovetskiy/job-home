fcd7b73d45f823d6de1d09557746136fa9e5f221
Artem Makovetskiy <makovetsky-artem@yandex.ru>
2025-07-31 22:57:50 +0300

    game over?

--- game.py
+++ game.py
@@ -1,49 +1,74 @@
 from gameparts import Board
 
-from gameparts.exceptions import FieldIndexError
+from gameparts.exceptions import CellOccupiedError, FieldIndexError
 
 def main():
     game = Board()
+    current_player = 'X'
+    running = True
     game.display()
 
-    # Запускается бесконечный цикл.
-    while True:
-        # В этом блоке содержатся операции, которые могут вызвать исключение.
-        try:
-            # Пользователь вводит значение номера строки.
-            row = int(input('Введите номер строки: '))
-            # Если введённое число меньше 0 или больше
-            # или равно game.field_size...
-            if row < 0 or row >= game.field_size:
-                # ...выбрасывается собственное исключение FieldIndexError.
-                raise FieldIndexError
-            column = int(input('Введите номер столбца: '))
-            # Если введённое число меньше 0 или больше
-            # или равно game.field_size...
-            if column < 0 or column >= game.field_size:
-                # ...выбрасывается собственное исключение FieldIndexError.
-                raise FieldIndexError
-        # Если возникает исключение FieldIndexError...
-        except FieldIndexError:
-            # ...выводятся сообщения...
-            print(
-                'Значение должно быть неотрицательным и меньше '
-                f'{game.field_size}.'
-            )
-            print('Пожалуйста, введите значения для строки и столбца заново.')
-            # ...и цикл начинает свою работу сначала,
-            # предоставляя пользователю ещё одну попытку ввести данные.
-            continue
-        # Если в блоке try исключения не возникло...
-        else:
-            # ...значит, введённые значения прошли все проверки
-            # и могут быть использованы в дальнейшем.
-            # Цикл прерывается.
-            break
+    while running:
 
-    game.make_move(row, column, 'X')
-    print('Ход сделан!')
-    game.display()
+        print(f'Ход делают {current_player}')
+
+        # Запускается бесконечный цикл.
+        while True:
+            # В этом блоке содержатся операции, которые могут вызвать исключение.
+            try:
+                # Пользователь вводит значение номера строки.
+                row = int(input('Введите номер строки: '))
+                # Если введённое число меньше 0 или больше
+                # или равно game.field_size...
+                if row < 0 or row >= game.field_size:
+                    # ...выбрасывается собственное исключение FieldIndexError.
+                    raise FieldIndexError
+                column = int(input('Введите номер столбца: '))
+                # Если введённое число меньше 0 или больше
+                # или равно game.field_size...
+                if column < 0 or column >= game.field_size:
+                    # ...выбрасывается собственное исключение FieldIndexError.
+                    raise FieldIndexError
+                if game.board[row][column] != ' ':
+                    raise CellOccupiedError
+            # Если возникает исключение FieldIndexError...
+            except FieldIndexError:
+                # ...выводятся сообщения...
+                print(
+                    'Значение должно быть неотрицательным и меньше '
+                    f'{game.field_size}.'
+                )
+                print('Пожалуйста, введите значения для строки и столбца заново.')
+                # ...и цикл начинает свою работу сначала,
+                # предоставляя пользователю ещё одну попытку ввести данные.
+                continue
+            except CellOccupiedError:
+                print('Ячейка занята')
+                print('Введите другие координаты.')
+            # Если в блоке try исключения не возникло...
+            except ValueError:
+                print('Буквы вводить нельзя. Только числа.')
+                print('Пожалуйста, введите значения для строки и столбца заново.')
+                continue
+            except Exception as e:
+                print(f'Возникла ошибка: {e}')
+
+            else:
+                # ...значит, введённые значения прошли все проверки
+                # и могут быть использованы в дальнейшем.
+                # Цикл прерывается.
+                break
+
+        game.make_move(row, column, current_player)
+        game.display()
+        if game.check_win(current_player):
+            print(f'Победили {current_player}.')
+            running = False
+        elif game.is_board_full():
+            print('Ничья!')
+            running = False
+            
+        current_player = "O" if current_player == 'X' else "X"
 
 if __name__ == '__main__':
     main()
